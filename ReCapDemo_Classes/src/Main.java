067f9287a413ae28b7e70b5bf60b06d9270410c0
Batuhan Baykal <batuhanbaykal666@gmail.com>
2022-10-05 16:27:43 +0200

    ReCapDemo_Classes, classes and classesWithAttributes projects are added.

---
+++ ReCapDemo_Classes/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Topla(30,40);
+        System.out.println(sonuc);
+    }
+}
