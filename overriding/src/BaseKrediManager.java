4328230df69df7a498bdda3a801c43cda814b546
Batuhan Baykal <batuhanbaykal666@gmail.com>
2022-10-12 13:24:25 +0200

    overriding project is added.

---
+++ overriding/src/BaseKrediManager.java
@@ -0,0 +1,5 @@
+public class BaseKrediManager {
+    public double hesapla(double tutar){   //public final double hesapla(double tutar) dersen override i engellemis olursun
+        return tutar * 1.18;
+    }
+}
